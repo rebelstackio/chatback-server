6a8b1f2d2e7c105d6c1c4b83f0a5118e7c00f846
Javier <jegj57@gmail.com>
2017-04-17 17:42:24 -0500

    Add cors option to browser get access to auth header

--- server/app.js
+++ server/app.js
@@ -21,6 +21,7 @@ const allowCrossDomain = function(req, res, next) {
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,PATCH,DELETE,OPTIONS');
 	res.header('Access-Control-Allow-Headers', 'Accept,Content-Type, Authorization, Content-Length, X-Requested-With');
+	res.header('Access-Control-Expose-Headers', 'Accept,Content-Type, Authorization, Content-Length, X-Requested-With');
 	next();
 };
 
